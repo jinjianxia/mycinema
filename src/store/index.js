8d37a0318fe7cca099e7ca8166520a3013c1d3c0
jinjianxia <jianxia1999@gmail.com>
2021-12-20 19:56:28 +0800

    commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,18 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+// import recommend from "@/store/recommend";
+import screening from "@/store/screening";
+import recommend from "@/store/recommend";
+import cinema from "@/store/cinema";
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+  state: {},
+  mutations: {},
+  actions: {},
   modules: {
+    recommend,
+    screening,
+    cinema
   }
 })
